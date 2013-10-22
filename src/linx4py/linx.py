3eb67414706285c063e2a69ba86e11b144237933
ba2150 <bjorn.arnelid@xdin.com>
2013-10-22 08:23:31 +0200

    Added linx.py and renamed linx_test.py

---
+++ src/linx4py/linx.py
@@ -0,0 +1,17 @@
+'''
+Created on 22 okt 2013
+
+@author: Bjorn Arnelid
+'''
+
+class Linx(object):
+    '''
+    Class for communicating with other systems through Linx signals. 
+    '''
+
+
+    def __init__(self,params):
+        '''
+        Constructor
+        '''
+        pass
