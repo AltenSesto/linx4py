fe317743057c79c60ef38059d31f3631f8773325
ba2150 <bjorn.arnelid@xdin.com>
2013-10-25 11:47:18 +0200

    Added Jenkins run script

---
+++ jenkins.sh
@@ -0,0 +1,17 @@
+PYTHNPATH = ${PYTHONPATH}:./src/linx4py
+python src/test/linxAdapterTest.py
+python src/test/linxTest.py
+
+pylint --output-format=parseable src/*.py | tee pylint_out.txt
+pep8 src/*.py | tee pep8_out.txt
+clonedigger --cpd-output src/*.py -o clonedigger_out.xml
+sloccount --duplicates --wide --details src/ > sloccount_out.txt
+pymetrics src/*.py > complexity.txt
+pycabehtml -i complexity.txt -o complexity.html -a complexity_acc.txt -g output.png
+#pyreverse -A -S -o png -p GameOfLife game_of_life/*.py
+
+coverage run src/test/linxAdapterTest.py
+coverage run src/test/linxTest.py
+coverage xml
+
+doxygen Doxyfile
