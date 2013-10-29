dd70d5d9f0fd5b80cb991f7cd572cc35857a6131
ba2150 <bjorn.arnelid@xdin.com>
2013-10-29 09:04:54 +0100

    added tests to jenkins

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,8 @@
 export PYTHONPATH="${PYTHONPATH}:./src"
 python src/test/linxAdapterTest.py
 python src/test/linxTest.py
+python src/test/asyncRecieverTest.py
+python src/test/signalCollectionTest.py
 
 pylint --output-format=parseable src/linx4py/*.py | tee pylint_out.txt
 pep8 src/*.py | tee pep8_out.txt
@@ -12,6 +14,8 @@ pycabehtml -i complexity.txt -o complexity.html -a complexity_acc.txt -g output.
 
 coverage run src/test/linxAdapterTest.py
 coverage run src/test/linxTest.py
+coverage run src/test/asyncRecieverTest.py
+coverage run src/test/signalCollectionTest.py
 coverage xml
 
 doxygen Doxyfile
