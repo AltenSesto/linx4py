e7d11e347b517b5933adba44a77eb17133947f25
ba2150 <bjorn.arnelid@xdin.com>
2013-11-07 10:14:13 +0100

    omitting __init__.py and simplified coerage run

--- jenkins.sh
+++ jenkins.sh
@@ -13,11 +13,7 @@ pymetrics src/linx4py/*.py > complexity.txt
 pycabehtml -i complexity.txt -o complexity.html -a complexity_acc.txt -g output.png
 #pyreverse -A -S -o png -p GameOfLife game_of_life/*.py
 
-coverage run src/test/linx_wrapper_test.py 
-coverage run -a src/test/linx_adapter_test.py 
-coverage run -a src/test/linx_test.py 
-coverage run -a src/test/async_receiver_test.py 
-coverage run -a src/test/signal_collection_test.py 
-coverage xml
+coverage run --source=src/test
+coverage xml --omit="*__init__.py"
 
 doxygen Doxyfile
